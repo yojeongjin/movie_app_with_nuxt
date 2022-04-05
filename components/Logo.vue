9403bd3b41332bcee5cb833fe0833950829eaaff
yojeongjin <osolku@naver.com>
2022-04-05 22:29:14 +0900

    add nuxt project

---
+++ components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtrLink
+    to="/"
+    class="logo">
+    <span>OMDbAPI</span>.COM
+  </NuxtrLink>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;
+  text-decoration: none;
+  &:hover {
+    color: $black;
+  }
+  span {
+    color: $primary;
+  }
+}
+</style>
---
+++ store/movie.js
@@ -0,0 +1,113 @@
+import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
+
+const _defaultMessage = 'Search for the movie title!'
+
+export default {
+  namespaced: true,
+
+  state: () => ({
+    movies: [],
+    message: _defaultMessage,
+    loading: false,
+    theMovie: {}
+  }),
+
+  getters: {},
+
+  mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach(key => {
+        state[key] = payload[key]
+      })
+    },
+    resetMovies(state) {
+      state.movies = []
+      state.message = _defaultMessage
+      state.loading = false
+    }
+  },
+
+
+  actions: {
+    async searchMovies({ state, commit }, payload) {
+      if (state.loading) return
+
+      commit('updateState', {
+        message: '',
+        loading: true
+      })
+
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1
+        })
+        const { Search, totalResults } = res.data
+        commit('updateState', {
+          movies: _uniqBy(Search, 'imdbID')
+        })
+
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10)
+
+        // page 추가 요청
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page += 1) {
+            if (page > (payload.number / 10)) break
+            const res = await _fetchMovie({
+              ...payload,
+              page
+            })
+            const { Search } = res.data
+            commit('updateState', {
+              movies: _uniqBy([
+                ...state.movies,
+                ...Search
+              ], 'imdbID')
+            })
+          }
+        }
+      } catch ({ message }) {
+        commit('updateState', {
+          movies: [],
+          message
+        })
+      } finally {
+        commit('updateState', {
+          loading: false
+        })
+      }
+    },
+    async searchMovieWithId({ state, commit }, payload) {
+      if (state.loading) return
+
+      commit('updateState', {
+        theMovie: {},
+        loading: true,
+      })
+
+      try {
+        const res = await _fetchMovie(payload)
+        commit('updateState', {
+          theMovie: res.data
+        })
+      } catch (error) {
+        commit('updateState', {
+          theMovie: {}
+        })
+      } finally {
+        commit('updateState', {
+          loading: false
+        })
+      }
+    }
+  }
+}
+
+async function _fetchMovie(payload) {
+  const url = process.client
+    ? '/api/movie'
+    : `${process.env.CLIENT_URL}/api/movie`
+  return await axios.post(url, payload)
+}
